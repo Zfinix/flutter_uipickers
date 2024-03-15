87cbd601f1664fab421d459e303544983a16acc8
Chiziaruhoma <chiziaruhoma@gmail.com>
2024-03-15 12:26:51 +0100

    fixed inDateFormatter

--- ios/Classes/FLDatePicker.swift
+++ ios/Classes/FLDatePicker.swift
@@ -10,8 +10,10 @@ class FLDatePicker: NSObject, FlutterPlatformView {
 
     static let inDateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX") // Use POSIX to avoid unexpected behavior
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
-        return dateFormatter
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return dateFormatter // Adjust if necessary for your use case
     }()
 
     init(
@@ -20,72 +22,79 @@ class FLDatePicker: NSObject, FlutterPlatformView {
         arguments args: Any?,
         binaryMessenger messenger: FlutterBinaryMessenger?
     ) {
-        let argsDict = args as! Dictionary<String, Any>;
-        let picker = UIDatePicker(frame: frame)
-        picker.preferredDatePickerStyle = .compact
-        picker.datePickerMode = UIDatePicker.Mode(rawValue: argsDict["mode"] as! Int)!
-        if let dateStr = argsDict["date"] as? String {
-            picker.date = FLDatePicker.inDateFormatter.date(from: dateStr) ?? Date()
-        }
-        if let dateStr = argsDict["minimumDate"] as? String {
-            picker.minimumDate = FLDatePicker.inDateFormatter.date(from: dateStr) ?? Date()
-        }
-        if let dateStr = argsDict["maximumDate"] as? String {
-            picker.maximumDate = FLDatePicker.inDateFormatter.date(from: dateStr) ?? Date()
-        }
-        if let tintColorStr = argsDict["tintColor"] as? String {
-            picker.tintColor = UIColor(hexString: tintColorStr)
-        }
-        if let backgroundColorStr = argsDict["backgroundColor"] as? String {
-            if let bgView = picker.subviews.first?.subviews.first?.subviews.first {
-                bgView.backgroundColor = UIColor(hexString: backgroundColorStr)
-                backgroundColor = bgView.backgroundColor
+        do{
+            let argsDict = args as! Dictionary<String, Any>;
+            //print("date: \(argsDict)")
+            
+            let picker = UIDatePicker(frame: frame)
+            picker.preferredDatePickerStyle = .compact
+            picker.datePickerMode = UIDatePicker.Mode(rawValue: argsDict["mode"] as! Int)!
+            if let dateStr = argsDict["date"] as? String {
+                picker.date = FLDatePicker.inDateFormatter.date(from: dateStr) ?? Date()
             }
-        }
-        if let borderColorStr = argsDict["borderColor"] as? String {
-            if let bgView = picker.subviews.first?.subviews.first?.subviews.first {
-                bgView.layer.borderColor = UIColor(hexString: borderColorStr)?.cgColor
+            if let dateStr = argsDict["minimumDate"] as? String {
+                picker.minimumDate = FLDatePicker.inDateFormatter.date(from: dateStr) ?? Date()
             }
-        }
-        if let borderWidth = argsDict["borderWidth"] as? CGFloat {
-            if let bgView = picker.subviews.first?.subviews.first?.subviews.first {
-                bgView.layer.borderWidth = borderWidth
+            if let dateStr = argsDict["maximumDate"] as? String {
+               picker.maximumDate = FLDatePicker.inDateFormatter.date(from: dateStr) ?? Date()
+
             }
-        }
-        if let cornerRadius = argsDict["cornerRadius"] as? CGFloat {
-            if let bgView = picker.subviews.first?.subviews.first?.subviews.first {
-                bgView.layer.cornerRadius = cornerRadius
+            if let tintColorStr = argsDict["tintColor"] as? String {
+                picker.tintColor = UIColor(hexString: tintColorStr)
             }
-        }
-        if let fontSize = argsDict["fontSize"] as? CGFloat {
-            if let lblView = picker.subviews.first?.subviews.first?.subviews[1] {
-                lblView.setValue(UIFont.systemFont(ofSize: fontSize), forKey: "font")
-            }
-        }
-        if let textColorStr = argsDict["textColor"] as? String  {
-            if let lblView = picker.subviews.first?.subviews.first?.subviews[1] {
-                textColor = UIColor(hexString: textColorStr)
-                lblView.setValue(textColor, forKey: "textColor")
-            }
-        }
-        picker.sizeToFit()
-        _view = picker
-        channel = FlutterMethodChannel(name: "FLDatePicker/\(viewId)", binaryMessenger: messenger!);
-        super.init()
-        picker.addTarget(self, action: #selector(onChanged), for: .valueChanged)
-        picker.addTarget(self, action: #selector(onEndEdit), for: .editingDidEnd)
-        channel?.setMethodCallHandler({ call, reault in
-            if call.method == "setDate" {
-                if let date = call.arguments as? String {
-                    self._view.date = FLDatePicker.inDateFormatter.date(from: date) ?? Date()
+            if let backgroundColorStr = argsDict["backgroundColor"] as? String {
+                if let bgView = picker.subviews.first?.subviews.first?.subviews.first {
+                    bgView.backgroundColor = UIColor(hexString: backgroundColorStr)
+                    backgroundColor = bgView.backgroundColor
                 }
             }
-            if call.method == "setMinDate" {
-                if let dateStr = call.arguments as? String {
-                    self._view.minimumDate = FLDatePicker.inDateFormatter.date(from: dateStr) ?? Date()
+            if let borderColorStr = argsDict["borderColor"] as? String {
+                if let bgView = picker.subviews.first?.subviews.first?.subviews.first {
+                    bgView.layer.borderColor = UIColor(hexString: borderColorStr)?.cgColor
                 }
             }
-        })
+            if let borderWidth = argsDict["borderWidth"] as? CGFloat {
+                if let bgView = picker.subviews.first?.subviews.first?.subviews.first {
+                    bgView.layer.borderWidth = borderWidth
+                }
+            }
+            if let cornerRadius = argsDict["cornerRadius"] as? CGFloat {
+                if let bgView = picker.subviews.first?.subviews.first?.subviews.first {
+                    bgView.layer.cornerRadius = cornerRadius
+                }
+            }
+            if let fontSize = argsDict["fontSize"] as? CGFloat {
+                if let lblView = picker.subviews.first?.subviews.first?.subviews[1] {
+                    lblView.setValue(UIFont.systemFont(ofSize: fontSize), forKey: "font")
+                }
+            }
+            if let textColorStr = argsDict["textColor"] as? String  {
+                if let lblView = picker.subviews.first?.subviews.first?.subviews[1] {
+                    textColor = UIColor(hexString: textColorStr)
+                    lblView.setValue(textColor, forKey: "textColor")
+                }
+            }
+            picker.sizeToFit()
+            _view = picker
+            channel = FlutterMethodChannel(name: "FLDatePicker/\(viewId)", binaryMessenger: messenger!);
+            super.init()
+            picker.addTarget(self, action: #selector(onChanged), for: .valueChanged)
+            picker.addTarget(self, action: #selector(onEndEdit), for: .editingDidEnd)
+            channel?.setMethodCallHandler({ call, reault in
+                if call.method == "setDate" {
+                    if let date = call.arguments as? String {
+                       self._view.date = FLDatePicker.inDateFormatter.date(from: date) ?? Date()
+                    }
+                }
+                if call.method == "setMinDate" {
+                    if let dateStr = call.arguments as? String {
+                        self._view.minimumDate = FLDatePicker.inDateFormatter.date(from: dateStr) ?? Date()
+                    }
+                }
+            })
+        } catch let error {
+            print("\(error)")
+        }
 
     }
 
